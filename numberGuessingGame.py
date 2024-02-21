0cde456c2bd6e3124438905331b990e82ee7ff81
Lianna <liannapyman@gmail.com>
2024-02-21 21:18:23 +1030

    Adds logic for user to input number and respond to computer's guesses

---
+++ numberGuessingGame.py
@@ -0,0 +1,29 @@
+import random
+from random import choice
+
+correctGuess = False
+
+userNum = int(input("Enter a number between 1-100: "))
+
+previousGuesses = []
+
+compGuess = choice([i for i in range(0,100) if i not in previousGuesses])
+
+
+while correctGuess == False:
+    userInput = input("Is your number " + str(compGuess) + "? (Higher/Lower/Y)")
+
+    if userInput == "Higher":
+        newGuess = random.randint(compGuess, 100)
+        previousGuesses.append(newGuess)
+        compGuess = newGuess
+
+    elif userInput == "Lower":
+        newGuess = random.randint(1, compGuess)
+        previousGuesses.append(newGuess)
+        compGuess = newGuess
+    else:
+        newGuess = compGuess
+        print("Your number was " + str(newGuess) + "!")
+        correctGuess == True
+        break
